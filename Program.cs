f6b67ab11850fed93bc0ac0d805266b95268d4de
unknown <eliascrevatin@hotmail.com>
2022-07-18 19:20:27 -0300

    Primera subida-condicionales1

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Ejercicio25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float NotaPrimerExamen, NotaSegundoExamen, NotaTercerExamen;
+            float Promedio;
+
+
+            Console.WriteLine("Cuanto te sacaste en el primer examen?");
+            NotaPrimerExamen = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Cuanto te sacaste en el segundo?");
+            NotaSegundoExamen = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("y en el tercer examen?");
+            NotaTercerExamen = float.Parse(Console.ReadLine());
+
+            Promedio = (NotaPrimerExamen + NotaSegundoExamen + NotaTercerExamen) / 3; 
+
+            Console.WriteLine("Su promedio es de: " + Promedio.ToString("0.00"));
+        }
+    }
+}
